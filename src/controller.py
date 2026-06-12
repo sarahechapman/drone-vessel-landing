b8875c5303633058144a88ad024e01f1d3cb970e
Sarah <sarahechapman@hotmail.com>
2026-06-12 22:05:06 +0100

    Fix derivative spike on initialisation - smooth descent now achieved

--- src/controller.py
+++ src/controller.py
@@ -14,8 +14,12 @@ class PIDController(Controller):
         self.Kd = Kd
         self.previous_error = 0.0
         self.integral = 0.0
+        self.initialised = False
 
     def compute(self, target_position, current_position, dt):
+        if not self.initialised:
+            self.previous_error = target_position - current_position
+            self.initialised = True
         error = target_position-current_position
         force = self.Kp * error
         force += self.Kd * (error - self.previous_error) / dt
