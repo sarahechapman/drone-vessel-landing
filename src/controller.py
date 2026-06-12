a97622f4ef156d78dd4b0367cb7310019422f96f
Sarah <sarahechapman@hotmail.com>
2026-06-12 14:38:26 +0100

    Add Controller base class and PIDController

---
+++ src/controller.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Controller(ABC):
+    
+    @abstractmethod
+    def compute(self, current, target, dt):
+        pass
+
+class PIDController(Controller):
+    def __init__(self, Kp, Ki, Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.previous_error = 0.0
+        self.integral = 0.0
+
+    def compute(self, target_position, current_position, dt):
+        error = target_position-current_position
+        force = self.Kp * error
+        force += self.Kd * (error - self.previous_error) / dt
+        self.integral += error
+        force += self.Ki * self.integral * dt
+        self.previous_error = error
+        return force
